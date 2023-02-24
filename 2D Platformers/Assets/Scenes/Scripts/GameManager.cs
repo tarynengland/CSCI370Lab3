feec736506f938b6a6410c950835ec39ba6b9811
Taryn England <tarynengland1@yahoo.com>
2023-02-23 22:09:50 -0600

    Still get game manager to work with different scenes

--- 2D Platformers/Assets/Scenes/Scripts/GameManager.cs
+++ 2D Platformers/Assets/Scenes/Scripts/GameManager.cs
@@ -1,9 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+
+    public static GameManager Instance {get; private set;}
+    public GameObject Samplescene;
+    public GameObject curtain;
+    private bool raiseLower = false;
+    public GameObject canvas;
+    public GameObject eventSystem;
+
+
+    void Awake(){
+        if (Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(canvas);
+            DontDestroyOnLoad(eventSystem);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator ColorLerp(bool fadeouts, float duration){
+        float time = 0;
+        raiseLower = true;
+        Image curtainImg = curtain.GetComponent<Image>();
+        Color startValue;
+        Color endValue;
+        if (fadeouts) {
+            startValue = new Color(0,0,0,0);
+            endValue = new Color(0,0,0,1);
+        } else {
+            startValue = new Color(0,0,0,1);
+            endValue = new Color(0,0,0,0);
+        }
+        while (time < duration){
+            curtainImg.color = Color.Lerp(startValue,endValue, time/duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        curtainImg.color = endValue;
+        raiseLower = false;
+    }
+
+    IEnumerator LoadAsyncScene(string scene){
+        StartCoroutine(ColorLerp(true, 1));
+        while (raiseLower){
+            yield return null;
+        }
+        AsyncOperation asyncload = SceneManager.LoadSceneAsync(scene);
+        while (!asyncload.isDone){
+            yield return null;
+        }
+        StartCoroutine(ColorLerp(false, 1));
+    }
+
+    public void StartGame(){
+        StartCoroutine(LoadAsyncScene("main"));
+        Samplescene.SetActive(false);
+    }
+
+    public void HowToPlay(){
+        StartCoroutine(LoadAsyncScene("howtoplay"));
+        Samplescene.SetActive(false);
+    }
+    public void Credits(){
+        StartCoroutine(LoadAsyncScene("credit"));
+        Samplescene.SetActive(false);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
