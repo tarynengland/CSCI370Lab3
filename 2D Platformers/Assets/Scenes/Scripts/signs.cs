404c1ae4fe7a874a7c7fe2143bc659a31313be85
Dalton C <caseyde@hendrix.edu>
2023-02-24 19:14:24 -0600

    Finished Sign dialogue and made a dialogue manager

--- 2D Platformers/Assets/Scenes/Scripts/signs.cs
+++ 2D Platformers/Assets/Scenes/Scripts/signs.cs
@@ -6,6 +6,54 @@ using TMPro;
 
 public class signs : MonoBehaviour
 {
+
+
+
+    public GameObject dialogueBox;
+    public TextMeshProUGUI dialogueText;
+    public string text;
+    // when player collides with signs will bring up dialogue
+    public void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        print("Entered..");
+        if (collider2D.gameObject.CompareTag("Player"))
+        {
+            DialogueShow(text);
+        }
+    }
+    //hides dialogue on exit
+    public void OnTriggerExit2D(Collider2D collider2D)
+    {
+        if (collider2D.gameObject.CompareTag("Player"))
+        {
+            DialogueHide();
+        }
+    }
+
+    public void DialogueShow(string text)
+    {
+        dialogueBox.SetActive(true);
+        StopAllCoroutines();
+        StartCoroutine(TypeText(text));
+    }
+
+    public void DialogueHide()
+    {
+        dialogueBox.SetActive(false);
+    }
+
+    IEnumerator TypeText(string text)
+    {
+        dialogueText.text = "";
+        foreach (char c in text.ToCharArray())
+        {
+            dialogueText.text += c;
+            yield return new WaitForSeconds(0.02f);
+        }
+        //DialogHide();
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
